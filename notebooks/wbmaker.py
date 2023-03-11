c70bbbd8e9af0938268efda163898591202eb093
Sky Bristol <sbristol@usgs.gov>
2023-03-10 18:30:29 -0600

    Cleanup tasks and new county process notebook
    I'm at the point now where I can move ahead on a few remaining reference sources and then processing all FRS facilities.

--- notebooks/wbmaker.py
+++ notebooks/wbmaker.py
@@ -6,42 +6,41 @@ from urllib.parse import parse_qs
 from wikibaseintegrator.wbi_config import config as wbi_config
 from wikibaseintegrator import WikibaseIntegrator, wbi_login
 from wikibaseintegrator import models, datatypes
-from nested_lookup import nested_lookup
 
 class WikibaseConnection:
-    def __init__(self, bot_name: str):
+    def __init__(
+        self, 
+        bot_name: str,
+        authenticate: bool = True):
         if "SPARQL_ENDPOINT_URL" not in os.environ:
             raise ValueError("Environment does not appear to contain required variables to run this code.")
             
+        # Set basic parameters from env variables we have to know about to start operating
         self.sparql_endpoint = os.environ['SPARQL_ENDPOINT_URL']
         self.wikibase_url = os.environ['WIKIBASE_URL']
-        
-        # Hard assumed values
-        # Knowing these values in advance saves some time
-        self.prop_instance_of = 'P1'
-        self.prop_subclass_of = 'P2'
-        self.class_dataset = 'Q11'
-        self.prop_query_string = 'P29'
-        self.prop_html_table = 'P31'
-        self.prop_classifier = 'P35'
-        self.prop_declaration = 'P37'
-        
-        self.models = models
-        self.datatypes = datatypes
 
+        # Get properties and classes so we can ask for things by name and get the IDs
+        self.wb_properties, self.prop_lookup = self.properties()
+        self.wb_classes, self.class_lookup = self.classification()
+        
         # WikibaseIntegrator config
         wbi_config['MEDIAWIKI_API_URL'] = os.environ['MEDIAWIKI_API_URL']
         wbi_config['SPARQL_ENDPOINT_URL'] = os.environ['SPARQL_ENDPOINT_URL']
         wbi_config['WIKIBASE_URL'] = os.environ['WIKIBASE_URL']
         wbi_config['USER_AGENT'] = f'{bot_name}/1.0 ({os.environ["WIKIBASE_URL"]})'
         
+        # Instantiate important aspect of WBI for calling from elsewhere
+        self.models = models
+        self.datatypes = datatypes
+
         # Establish authentication connection to instance
         login_instance = wbi_login.Login(
             user=os.environ['BOT_NAME'],
             password=os.environ['BOT_PASS']
         )
-        self.wbi = WikibaseIntegrator(login=login_instance)        
-
+        self.wbi = WikibaseIntegrator(login=login_instance) 
+        
+                    
     # Parameters
     def sparql_namespaces(self):
         namespaces = """
@@ -52,22 +51,8 @@ class WikibaseConnection:
         return namespaces
 
     # Core Functions
-    def item_by_label(self, label: str):
-        label_query = """
-        SELECT ?item
-        WHERE {
-            ?item ?label "%s"@en .
-        }
-        """ % label
-        
-        results = self.sparql_query(
-            query=label_query,
-            output="raw"
-        )
-        
-        return results
-    
     def properties(self):
+        # Returns properties based on simple SPARQL query not requiring a priori information
         prop_query = """
         %s
 
@@ -83,12 +68,15 @@ class WikibaseConnection:
             output='dataframe'
         )
         
-        df['pid'] = df.property.apply(lambda x: x.split('/')[-1])
+        df['pid'] = df.property.apply(self.extract_wbid)
         df['p_type'] = df.property_type.apply(lambda x: x.split('#')[-1])
-        
-        return df
 
-    def classification(self, output: str = 'lookup'):
+        prop_lookup = df.set_index('propertyLabel')['pid'].to_dict()
+        
+        return df, prop_lookup
+
+    def classification(self):
+        # Pulls items that are subclasses, meaning they serve as classifiers in our Wikibase
         class_query = """
         %(namespaces)s
 
@@ -99,26 +87,32 @@ class WikibaseConnection:
         }
         """ % {
             'namespaces': self.sparql_namespaces(),
-            'prop_subclass': self.prop_subclass_of
+            'prop_subclass': self.prop_lookup['subclass of']
         }
         
-        return self.sparql_query(
+        df = self.sparql_query(
             query=class_query,
-            output=output
+            output='dataframe'
         )
+        df['qid'] = df['item'].apply(self.extract_wbid)
+
+        class_lookup = df.set_index('itemLabel')['qid'].to_dict()
+
+        return df, class_lookup
     
-    def datasets(self):
+    def datasets(self, output):
         class_query = """
         %(namespaces)s
 
         SELECT ?item ?itemLabel 
         WHERE {
-            ?item wdt:%(prop_subclass)s ?subclass.
+            ?item wdt:%(prop_subclass)s wd:%(class_dataset)s .
             SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
         }
         """ % {
             'namespaces': self.sparql_namespaces(),
-            'prop_subclass': self.prop_subclass_of
+            'prop_subclass': self.prop_lookup['instance of'],
+            'class_dataset': self.class_lookup['dataset']
         }
         
         return self.sparql_query(
@@ -126,11 +120,14 @@ class WikibaseConnection:
             output=output
         )
 
-    def datasource(self, ds_qid: str, output: str = 'dataframe'):
+    def datasource(self, ds_qid: str):
+        """
+        This is the somewhat complicated query for all properties and qualifiers on an item.
+        """
         ds_query = """
         %(namespaces)s
 
-        SELECT ?wdLabel ?ps_ ?ps_Label ?wdpqLabel ?pq_Label 
+        SELECT ?wdLabel ?ps_ ?ps_Label ?ps_type ?wdpq ?wdpqLabel ?pq_ ?pq_Label 
         {
           VALUES (?datasource) {(wd:%(ds_qid)s)}
 
@@ -145,6 +142,8 @@ class WikibaseConnection:
           ?wdpq wikibase:qualifier ?pq .
           }
 
+          OPTIONAL { ?ps_ wikibase:propertyType ?ps_type . }
+
           SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
         } ORDER BY ?datasourceLabel ?wd ?statement ?ps_
         """ % {
@@ -157,60 +156,79 @@ class WikibaseConnection:
             output="dataframe"
         )
         
-        if output == 'datatrame':
-            return ds_wb_source
-        
-        return self.format_datasource(ds_qid, ds_wb_source)
+        return ds_wb_source
 
-    def format_datasource(self, ds_qid, ds_wb_source):
-        d_config = {
-            ds_qid: {
-                'label_prop': None,
-                'description_prop': None,
-                'alias_prop': None,
-                'claims': []
-            }   
+    def item_by_label(self, label: str):
+        label_query = """
+        SELECT ?item
+        WHERE {
+            ?item ?label "%s"@en .
         }
+        """ % label
         
-        label_item = ds_wb_source[ds_wb_source.ps_Label == 'label']
-        if not label_item.empty:
-            d_config[ds_qid]['label_prop'] = label_item.iloc[0].pq_Label
+        results = self.sparql_query(
+            query=label_query,
+            output="raw"
+        )
+        
+        return results
+    
+    # def format_datasource(self, ds_qid, ds_wb_source):
+    #     # Formats the set of properties and qualifiers describing a dataset source for ease of use
+    #     d_config = {
+    #         ds_qid: {
+    #             'label_prop': None,
+    #             'description_prop': None,
+    #             'alias_prop': None,
+    #             'claims': []
+    #         }   
+    #     }
+        
+    #     label_item = ds_wb_source[ds_wb_source.ps_Label == 'label']
+    #     if not label_item.empty:
+    #         d_config[ds_qid]['label_prop'] = label_item.iloc[0].pq_Label
 
-        alias_item = ds_wb_source[ds_wb_source.ps_Label == 'alias']
-        if not alias_item.empty:
-            d_config[ds_qid]['alias_prop'] = alias_item.iloc[0].pq_Label
+    #     alias_item = ds_wb_source[ds_wb_source.ps_Label == 'alias']
+    #     if not alias_item.empty:
+    #         d_config[ds_qid]['alias_prop'] = alias_item.iloc[0].pq_Label
 
-        description_item = ds_wb_source[ds_wb_source.ps_Label == 'description']
-        if not description_item.empty:
-            d_config[ds_qid]['description_prop'] = description_item.iloc[0].pq_Label
+    #     description_item = ds_wb_source[ds_wb_source.ps_Label == 'description']
+    #     if not description_item.empty:
+    #         d_config[ds_qid]['description_prop'] = description_item.iloc[0].pq_Label
 
-        html_table = ds_wb_source[ds_wb_source.wdLabel == 'html table']
-        if not html_table.empty:
-            d_config[ds_qid]['interface_type'] = 'html table'
-            d_config[ds_qid]['interface_url'] = html_table.iloc[0].ps_
+    #     html_table = ds_wb_source[ds_wb_source.wdLabel == 'html table']
+    #     if not html_table.empty:
+    #         d_config[ds_qid]['interface_type'] = 'html table'
+    #         d_config[ds_qid]['interface_url'] = html_table.iloc[0].ps_
 
-        entity_classifier = ds_wb_source[ds_wb_source.wdLabel == 'entity classifier']
-        if not entity_classifier.empty:
-            d_config[ds_qid]['instance_of_qid'] = entity_classifier.iloc[0].ps_.split('/')[-1]
-            d_config[ds_qid]['instance_of_label'] = entity_classifier.iloc[0].ps_Label
+    #     entity_classifier = ds_wb_source[ds_wb_source.wdLabel == 'entity classifier']
+    #     if not entity_classifier.empty:
+    #         d_config[ds_qid]['instance_of_qid'] = entity_classifier.iloc[0].ps_.split('/')[-1]
+    #         d_config[ds_qid]['instance_of_label'] = entity_classifier.iloc[0].ps_Label
 
-        property_map_items = ds_wb_source[
-            (ds_wb_source.wdLabel == 'property from data source')
-            &
-            (~ds_wb_source.ps_Label.isin(['label','alias','description']))
-        ]
-        if not property_map_items.empty:
-            for index, row in property_map_items.iterrows():
-                d_config[ds_qid]['claims'].append({
-                    'pid': row.ps_.split('/')[-1],
-                    'label': row.ps_Label,
-                    'source_prop': row.pq_Label
-                })
+    #     property_map_items = ds_wb_source[
+    #         (ds_wb_source.wdLabel == 'property from data source')
+    #         &
+    #         (~ds_wb_source.ps_Label.isin(['label','alias','description']))
+    #     ]
+    #     if not property_map_items.empty:
+    #         for index, row in property_map_items.iterrows():
+    #             d_config[ds_qid]['claims'].append({
+    #                 'pid': row.ps_.split('/')[-1],
+    #                 'label': row.ps_Label,
+    #                 'source_prop': row.pq_Label
+    #             })
                 
-        return d_config
+    #     return d_config
     
     # Utilities
+    def extract_wbid(self, url):
+        # Extracts QID or PID from the end of a URL-formatted identifier
+        return url.split('/')[-1]
+        
     def sparql_query(self, query: str, endpoint: str = None, output: str = 'raw'):
+        # Run SPARQL query against target Wikibase instance of another endpoint
+        # Handles some processing of results needed often
         if not endpoint:
             endpoint = self.sparql_endpoint
         
@@ -254,6 +272,7 @@ class WikibaseConnection:
                 return data_records
 
     def parse_sparql_url(self, url: str, param: str = 'query'):
+        # Parse the query our of a SPARQL statement in URL form so we can run it in a different context
         x = urlparse(url)
         
         sparql_endpoint=f"{x.scheme}://{x.netloc}{x.path}"
@@ -261,66 +280,70 @@ class WikibaseConnection:
         
         return sparql_endpoint, sparql_query
     
-    def get_html_table(self, url: str, table_ordinal: int = 0):
+    def get_html_table(self, url: str, table_ordinal: int = 0, injection: dict = None):
+        # Retrieve tabular data from an HTML page
         tables_on_page = pd.read_html(url)
-        
+
         if not tables_on_page:
             return
-        
+
         # Build a converter to get everything as strings
         data_preview = tables_on_page[table_ordinal]
         converters = {c:str for c in data_preview.columns}
-        
+
+        # Retrieve the tables again using the converters
         tables_on_page = pd.read_html(url, converters=converters)
+
+        # Get specific table
+        df = tables_on_page[table_ordinal]
         
-        return tables_on_page[table_ordinal]
+        # Inject extra information if specified
+        if injection is not None:
+            for k, v in injection.items():
+                df[k] = v
 
-    def get_claims(self, qid: str):
-        existing_items_api = f"{os.environ['WIKIBASE_URL']}w/api.php?format=json&action=wbgetclaims&entity={qid}"
-        r = requests.get(existing_items_api)
+        return df
 
-        return nested_lookup('mainsnak', r.json())
+    # def simplify_claim_value(self, row):
+    #     if pd.notna(row['id']):
+    #         return row['id']
+        
+    #     if 'latitude' in row and pd.notna(row['latitude']):
+    #         return f"{str(row['latitude'])},{str(row['longitude'])}"
+        
+    #     return row[0]
     
-    def simplify_claim_value(self, row):
-        if pd.notna(row['id']):
-            return row['id']
+    # def qid_property_fetcher(self, qids: list):
+    #     df_qids = pd.DataFrame(list(set(qids)), columns=["qid"])
+    #     df_qids['claims'] = df_qids.qid.apply(self.get_claims)
+
+    #     df_qids_claims = df_qids.explode('claims').reset_index(drop=True)
+
+    #     df_qids_props = pd.concat([
+    #         df_qids_claims.drop(['claims'], axis=1), 
+    #         df_qids_claims['claims'].apply(pd.Series)
+    #     ], axis=1)
         
-        if 'latitude' in row and pd.notna(row['latitude']):
-            return f"{str(row['latitude'])},{str(row['longitude'])}"
+    #     df_qids_props.drop(columns=["snaktype","hash"], inplace=True)
         
-        return row[0]
+    #     df_qids_datavalues = pd.concat([
+    #         df_qids_props.drop(['datavalue'], axis=1), 
+    #         df_qids_props['datavalue'].apply(pd.Series)
+    #     ], axis=1)
+
+    #     df_qids_values = pd.concat([
+    #         df_qids_datavalues.drop(['value'], axis=1), 
+    #         df_qids_datavalues['value'].apply(pd.Series)
+    #     ], axis=1)
+        
+    #     df_qids_values['value'] = df_qids_values.apply(self.simplify_claim_value, axis=1)
+
+    #     return df_qids_values[["qid","property","value"]]
     
-    def qid_property_fetcher(self, qids: list):
-        df_qids = pd.DataFrame(list(set(qids)), columns=["qid"])
-        df_qids['claims'] = df_qids.qid.apply(self.get_claims)
-
-        df_qids_claims = df_qids.explode('claims').reset_index(drop=True)
-
-        df_qids_props = pd.concat([
-            df_qids_claims.drop(['claims'], axis=1), 
-            df_qids_claims['claims'].apply(pd.Series)
-        ], axis=1)
-        
-        df_qids_props.drop(columns=["snaktype","hash"], inplace=True)
-        
-        df_qids_datavalues = pd.concat([
-            df_qids_props.drop(['datavalue'], axis=1), 
-            df_qids_props['datavalue'].apply(pd.Series)
-        ], axis=1)
-
-        df_qids_values = pd.concat([
-            df_qids_datavalues.drop(['value'], axis=1), 
-            df_qids_datavalues['value'].apply(pd.Series)
-        ], axis=1)
-        
-        df_qids_values['value'] = df_qids_values.apply(self.simplify_claim_value, axis=1)
-
-        return df_qids_values[["qid","property","value"]]
-    
-    def item_collection_processor(self, dict_items):
-        if not isinstance(dict_items, dict):
-            return
-        all_items = []
-        for i, item_list in dict_items.items():
-            all_items.extend(item_list)
-        return all_items
+    # def item_collection_processor(self, dict_items):
+    #     if not isinstance(dict_items, dict):
+    #         return
+    #     all_items = []
+    #     for i, item_list in dict_items.items():
+    #         all_items.extend(item_list)
+    #     return all_items
